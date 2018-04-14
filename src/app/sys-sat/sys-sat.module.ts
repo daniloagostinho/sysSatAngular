5f1fb1a96be7de0d8f2ae984739d402c1b3097ab
Vitor Yugulis <vitor.yugulis@gmail.com>
2018-04-14 16:49:09 -0300

    Criado componentes Orcamentos e Ordem-servico

--- src/app/sys-sat/sys-sat.module.ts
+++ src/app/sys-sat/sys-sat.module.ts
@@ -3,6 +3,8 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SysSatComponent } from './sys-sat.component';
 import { EdificiosComponent } from './edificios/edificios.component';
+import { OrcamentosComponent } from './orcamentos/orcamentos.component';
+import { OrdemServicoComponent } from './ordem-servico/ordem-servico.component';
 
 
 @NgModule({
@@ -10,7 +12,7 @@ import { EdificiosComponent } from './edificios/edificios.component';
     CommonModule,
     AppComponent
   ],
-  declarations: [SysSatComponent, EdificiosComponent],
+  declarations: [SysSatComponent, EdificiosComponent, OrcamentosComponent, OrdemServicoComponent],
   schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class SysSatModule { }
