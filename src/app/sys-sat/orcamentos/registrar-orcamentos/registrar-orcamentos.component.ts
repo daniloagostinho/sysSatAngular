cc469ef03c16068895ba457b043133a4e89af678
Vitor Yugulis <vitor.yugulis@gmail.com>
2018-04-15 22:08:20 -0300

    Implementação do layout das ordens de serviço e orçamentos. Ajustes nos outros layouts

--- src/app/sys-sat/orcamentos/registrar-orcamentos/registrar-orcamentos.component.ts
+++ src/app/sys-sat/orcamentos/registrar-orcamentos/registrar-orcamentos.component.ts
@@ -14,6 +14,29 @@ export class RegistrarOrcamentosComponent implements OnInit {
     {value: '4', viewValue: 'Condomino'}
   ];
 
+  edificios = [
+    {value: '1', viewValue: 'Esmeralda'},
+    {value: '2', viewValue: 'Gramado XII'},
+    {value: '3', viewValue: 'Hawaii'},
+    {value: '4', viewValue: 'Elena'}
+  ];
+
+  funcionarios = [
+    {value: '1', viewValue: 'Marcos'},
+    {value: '2', viewValue: 'Marcus'},
+    {value: '3', viewValue: 'Paulo'},
+    {value: '4', viewValue: 'Rubens'}
+  ];
+
+  servico = [
+    {value: '1', viewValue: 'Interfone'},
+    {value: '2', viewValue: 'Portão automático'},
+    {value: '3', viewValue: 'Câmera'},
+    {value: '4', viewValue: 'Cerca elétrica'}
+  ];
+
+  valor: any;
+
   cpfMask = [ /[1-9]/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '.', /\d/, /\d/];
   dateMask = [ /\d/, /\d/, '/', /\d/, /\d/, '/', /\d/, /\d/, /\d/, /\d/]
 
--- src/app/sys-sat/ordem-servico/registrar-ordem-servico/registrar-ordem-servico.component.ts
+++ src/app/sys-sat/ordem-servico/registrar-ordem-servico/registrar-ordem-servico.component.ts
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrarOrdemServicoComponent implements OnInit {
 
+  funcoes = [
+    {value: '1', viewValue: 'Zelador'},
+    {value: '2', viewValue: 'Síndico'},
+    {value: '3', viewValue: 'Porteiro'},
+    {value: '4', viewValue: 'Condomino'}
+  ];
+
+  edificios = [
+    {value: '1', viewValue: 'Esmeralda'},
+    {value: '2', viewValue: 'Gramado XII'},
+    {value: '3', viewValue: 'Hawaii'},
+    {value: '4', viewValue: 'Elena'}
+  ];
+
+  funcionarios = [
+    {value: '1', viewValue: 'Marcos'},
+    {value: '2', viewValue: 'Marcus'},
+    {value: '3', viewValue: 'Paulo'},
+    {value: '4', viewValue: 'Rubens'}
+  ];
+
+  servico = [
+    {value: '1', viewValue: 'Interfone'},
+    {value: '2', viewValue: 'Portão automático'},
+    {value: '3', viewValue: 'Câmera'},
+    {value: '4', viewValue: 'Cerca elétrica'}
+  ];
+
+  valor: any;
+
+  cpfMask = [ /[1-9]/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '.', /\d/, /\d/];
+  dateMask = [ /\d/, /\d/, '/', /\d/, /\d/, '/', /\d/, /\d/, /\d/, /\d/]
+
   constructor() { }
 
   ngOnInit() {
   }
-
 }
